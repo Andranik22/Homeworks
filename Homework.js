07e256c5ac21930082ec15f772e1a26a5072f9d6
Andranik22 <85675641+Andranik22@users.noreply.github.com>
2021-06-12 23:56:22 +0400

    Add files via upload

---
+++ Homework.js
@@ -0,0 +1,130 @@
+1.Xndir
+
+let x= 12
+ 
+    y= -56
+
+
+x=x+y
+
+y=x-y
+
+x=x-y
+
+
+
+2.Xndir
+
+n = +prompt()
+
+let lD =  n%10
+
+let n1 = n/10 | 0
+
+if (lD == 0 || lD == n){
+ 
+  console.log(n)
+
+} else {
+
+console.log(Number(String(lD) + String(n1)))
+
+}
+
+3.Xndir
+
+3.1)
+let x = 45
+
+    y = -12
+
+    z = 0
+
+    a = 3
+
+    b = -15
+
+let sum = x + y + z + a + b
+
+let lenght = 5
+
+console.log(sum/lenght)
+
+3.2)
+let x = 7
+    y = 52
+
+    z = -23
+    a = 9
+    b = -81
+
+let sum = x + y + z + a + b
+
+let lenght = 5
+
+console.log(sum/lenght)
+
+4.Xndir
+
+4.1
+let n1 = 45
+
+    n2 = 26
+
+    n3 = 78
+if ( n1 < n2 && n2 < n3 ) {
+ 
+ console.log(n1 , n2 , n3)
+
+} else if (n1 > nb && n3 < n2 ) {
+
+console.log( n1 , n2 , n2)
+
+} else if ( n2 < n1 && n1 < n3 ) { 
+ 
+console.log( b , a , c)
+
+} else if ( n2 < n1 && n3 < n1 ) {
+ 
+console.log ( n2 , n3 , n1 )
+
+} else if ( n3 < n1 && n1 < n2 ) {
+
+ console.log ( n3 , n2 , n2)
+
+} else {
+ 
+console.log( n3 , n2 , n1 )
+
+}
+
+4.2
+
+let n1 = -23
+    n2 = -456
+    n3 = 0
+if ( n1 < n2 && n2 < n3 ) {
+ 
+ console.log(n1 , n2 , n3)
+
+} else if (n1 > nb && n3 < n2 ) {
+
+console.log( n1 , n2 , n2)
+
+} else if ( n2 < n1 && n1 < n3 ) { 
+ 
+console.log( b , a , c)
+
+} else if ( n2 < n1 && n3 < n1 ) {
+ 
+console.log ( n2 , n3 , n1 )
+
+} else if ( n3 < n1 && n1 < n2 ) {
+
+ console.log ( n3 , n2 , n2)
+
+} else {
+ 
+console.log( n3 , n2 , n1 )
+
+}
